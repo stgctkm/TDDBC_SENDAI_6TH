7d76cd73fa361bdac052efd0e1daecf6c981d868
setoguchi takuma <setoguchi.takuma@gmail.com>
2016-11-26 15:52:37 +0900

    課題1-2完了

--- src/fraction.js
+++ src/fraction.js
@@ -4,15 +4,35 @@ class Fraction {
     /**
      * 分子と分母を与えて分数のインスタンスを作成する。
      *
-     * @param numerator 分子。null不可。
-     * @param denominator 分母。null不可。0不可。
+     * @param numerator number 分子。0から9までの整数。null不可。
+     * @param denominator number 分母。0から9までの整数。null不可。0不可。
      */
-    constructor(numerator, denominator) {
-        assert(typeof numerator === 'number');
-        assert(typeof denominator === 'number');
-        assert(denominator !== 0);
-        this.numerator = numerator;
-        this.denominator = denominator;
+    constructor({numerator, denominator}) {
+        assert(Number.isInteger(numerator));
+        assert(0 <= numerator && numerator <= 9);
+        assert(Number.isInteger(denominator));
+        assert(0 < denominator && denominator <= 9);
+        this._numerator = numerator;
+        this._denominator = denominator;
+    }
+
+    get numerator() {
+        return this._numerator;
+    }
+
+    get denominator() {
+        return this._denominator;
+    }
+
+    /**
+     * 分数の文字列表記を返す
+     *
+     * @override
+     * @returns {string}
+     */
+    toString() {
+        return `${this.numerator}\n-\n${this.denominator}`;
     }
 }
+
 module.exports = Fraction;
--- test/fraction-test.js
+++ test/fraction-test.js
@@ -3,32 +3,87 @@
 const Fraction = require('../src/fraction');
 const assert = require('assert');
 
-describe('Fraction', () => {
+suite('分数を表すFractionクラス', () => {
 
-    it('分子を返す', () => {
-        const sut = new Fraction(4, 7);
-        assert(sut.numerator === 4);
-    });
-
-    it('分母を返す', () => {
-        const sut = new Fraction(4, 7);
-        assert(sut.denominator === 7);
-    });
-
-    it('numerator 引数は必須', () => {
-        assert.throws(() => {
-            new Fraction();
+    suite('分数 4/7 の場合', () => {
+        const sut = new Fraction({denominator: 7, numerator: 4});
+        test('分子は4', () => {
+            assert(sut.numerator === 4);
         });
-    });
-    it('denominator 引数は必須', () => {
-        assert.throws(() => {
-            new Fraction(4);
+        test('分母は7', () => {
+            assert(sut.denominator === 7);
         });
-    });
-    it('分母が0の時エラー', () => {
-        assert.throws(() => {
-            new Fraction(4, 0);
+        test('文字列表現', () => {
+            assert(sut.toString() ===
+`4
+-
+7`);
         });
     });
 
+    suite('分数 0/9 の場合', () => {
+        const sut = new Fraction({numerator: 0, denominator: 9});
+        test('分子は0', () => {
+            assert(sut.numerator === 0);
+        });
+        test('分母は9', () => {
+            assert(sut.denominator === 9);
+        });
+        test('文字列表現', () => {
+            assert(sut.toString() === '0\n-\n9');
+        });
+    });
+
+    suite('コンストラクタの事前条件', () => {
+        test('分子は必須', () => {
+            assert.throws(() => {
+                new Fraction({denominator: 7});
+            });
+        });
+        test('分子は number である事', () => {
+            assert.throws(() => {
+                new Fraction({numerator: '4', denominator: 7});
+            });
+        });
+        test('分母は必須', () => {
+            assert.throws(() => {
+                new Fraction({numerator: 4});
+            });
+        });
+        test('分母は number である事', () => {
+            assert.throws(() => {
+                new Fraction({numerator: 4, denominator: '7'});
+            });
+        });
+        test('分母が0の時エラー', () => {
+            assert.throws(() => {
+                new Fraction({numerator: 4, denominator: 0});
+            });
+        });
+        test('分子が-1の時はエラー', () => {
+            assert.throws(() => {
+                new Fraction({numerator: -1, denominator: 9});
+            });
+        });
+        test('分子が10の時はエラー', () => {
+            assert.throws(() => {
+                new Fraction({numerator: 10, denominator: 9});
+            });
+        });
+        test('分母が10の時はエラー', () => {
+            assert.throws(() => {
+                new Fraction({numerator: 6, denominator: 10});
+            });
+        });
+        test('分子が整数でない場合はエラー', () => {
+            assert.throws(() => {
+                new Fraction({numerator: 3.5, denominator: 7});
+            });
+        });
+        test('分母が整数でない場合はエラー', () => {
+            assert.throws(() => {
+                new Fraction({numerator: 3, denominator: 7.5});
+            });
+        });
+    });
 });
